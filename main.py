1f13587aed949ddaee2cdc3d42a003768d58746d
Gu, Park <doctorgu@kakao.com>
2025-02-20 08:52:39 +0900

    image added

--- main.py
+++ main.py
@@ -17,7 +17,15 @@ def main():
     while True:
         time.sleep(0.5)
 
-        full_paths = [f"{img_dir}youtube_skip.png", f"{img_dir}youtube_skip_poll.png"]
+        full_paths = [
+            f"{img_dir}youtube_skip.png",
+            f"{img_dir}youtube_skip_poll.png",
+            f"{img_dir}youtube_skip_transparent_dark_background.png",
+            f"{img_dir}youtube_skip_no_arrow_dark_background.png",
+            f"{img_dir}youtube_no_thanks.png",
+            f"{img_dir}youtube_no_thanks_2.png",
+            # f"{img_dir}youtube_dot_3.png",
+        ]
         ret = click_imgs(full_paths)
         if not ret:
             continue
