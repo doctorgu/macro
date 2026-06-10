91511c895d380abf24f1cdea769d9bce1c16e086
doctorgu <doctorgu@kakao.com>
2026-06-11 06:31:31 +0900

    moved images

--- youtube/click_skip.py
+++ youtube/click_skip.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 import time
 import pyautogui
-
+import os
 from util import click_imgs
 
 
@@ -13,18 +13,18 @@ def main():
     # config
     pyautogui.PAUSE = 0.5
 
-    img_dir = "./images/"
+    img_dir = os.path.join(os.path.dirname(__file__), "images")
 
     while True:
         time.sleep(0.5)
 
         full_paths = [
-            f"{img_dir}youtube_skip.png",
-            f"{img_dir}youtube_skip_poll.png",
-            f"{img_dir}youtube_skip_transparent_dark_background.png",
-            f"{img_dir}youtube_skip_no_arrow_dark_background.png",
-            f"{img_dir}youtube_no_thanks.png",
-            f"{img_dir}youtube_no_thanks_2.png",
+            os.path.join(img_dir, "youtube_skip.png"),
+            os.path.join(img_dir, "youtube_skip_poll.png"),
+            os.path.join(img_dir, "youtube_skip_transparent_dark_background.png"),
+            os.path.join(img_dir, "youtube_skip_no_arrow_dark_background.png"),
+            os.path.join(img_dir, "youtube_no_thanks.png"),
+            os.path.join(img_dir, "youtube_no_thanks_2.png"),
             # f"{img_dir}youtube_dot_3.png",
         ]
         ret = click_imgs(full_paths)
